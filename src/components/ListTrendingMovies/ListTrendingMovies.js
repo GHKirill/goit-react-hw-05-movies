b8a3dca36116e066e2e39fb8814e9a97b8b9de6e
Kirill Voronin <voroninkv09@gmail.com>
2023-04-28 18:46:25 +0300

    change ListTrendingMovies

--- src/components/ListTrendingMovies/ListTrendingMovies.js
+++ src/components/ListTrendingMovies/ListTrendingMovies.js
@@ -1,9 +1,13 @@
 import { useEffect, useState, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
 import { RotatingLines } from 'react-loader-spinner';
+import axios from 'axios';
 // import { fetchGetMoviesListTrend } from 'components/API/fetches';
-import { fetchGetMoviesListTrend } from '../API/fetches';
+//import { fetchGetMoviesListTrend } from '../API/fetches';
 import { MoviesList, MovieLink, MovieItem } from './ListTrendingMovies.styled';
+
+const KEY = '7fbdf6cf40f68e209048a77188ae45b3';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 
 export default function ListTrendingMovies() {
   const [listFilms, setListFilms] = useState([]);
@@ -17,12 +21,27 @@ export default function ListTrendingMovies() {
       return;
     }
     const getQuery = async function () {
-      try {
-        setLoader(true);
-        fetchGetMoviesListTrend(setListFilms, setLoader);
-      } catch (error) {
-        console.log(error);
-      }
+      // try {
+      //   setLoader(true);
+      //   fetchGetMoviesListTrend(setListFilms, setLoader);
+      // } catch (error) {
+      //   console.log(error);
+      // }
+      setLoader(true);
+      const fetchGetMoviesListTrend = async () => {
+        const response = await axios.get(`trending/movie/day?api_key=${KEY}`);
+        const result = response.data.results;
+
+        const workWithQuery = array => {
+          return array.map(({ id, title }) => ({
+            id,
+            title,
+          }));
+        };
+        setListFilms(workWithQuery(result));
+        setLoader(false);
+      };
+      fetchGetMoviesListTrend();
     };
     getQuery();
   }, []);
