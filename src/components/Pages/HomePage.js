3212dab2c7963c8f9dd3ed5412f2c0060ee2d198
Kirill Voronin <voroninkv09@gmail.com>
2023-04-28 16:16:54 +0300

    try fetch request

--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -1,4 +1,21 @@
 import ListTrendingFilms from '../ListTrendingFilms/ListTrendingFilms';
+import axios from 'axios';
+const KEY = '7fbdf6cf40f68e209048a77188ae45b3';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 export default function HomePage() {
   return <ListTrendingFilms />;
 }
+axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
+const fetchGetMoviesListTrend = async () => {
+  const response = await axios.get(`trending/movie/day?api_key=${KEY}`);
+  const result = response.data.results;
+  console.log(result);
+
+  // const workWithQuery = array => {
+  //   return array.map(({ id, title }) => ({
+  //     id,
+  //     title,
+  //   }));
+  // };
+};
+fetchGetMoviesListTrend();
